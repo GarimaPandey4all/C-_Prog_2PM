90b9d5ec33e950a4b207df8dd2d18ebae419697a
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-07-23 19:22:33 +0530

    Add files via upload

---
+++ Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿//using System; // namespace: is a collection of predefined classes
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args) // Main(): entry point or initial point of every C# program
+        {
+            //identifier() - function presentation
+
+            System.Console.WriteLine("Brain Mentors!"); // print output on the output screen
+            //string: more than one character
+            // 'a' - char
+            // "ab" - string
+        }
+    }
+}
