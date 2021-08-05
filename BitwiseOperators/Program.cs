6fb065956845afecc83ab08cf1470c8b5c6c15df
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-05 19:09:41 +0530

    Add files via upload

---
+++ BitwiseOperators/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BitwiseOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 32;
+            // b = 12;
+
+            //Console.WriteLine(a & b);
+            //Console.WriteLine(a | b);
+            //Console.WriteLine(a ^ b);
+            Console.WriteLine(a << 5);
+           // Console.WriteLine(a >> 5);
+        }
+    }
+}
