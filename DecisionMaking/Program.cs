3212093b6de9ae424102b5cde3642bb2c0868afa
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-07 14:14:07 +0530

    Add files via upload

---
+++ DecisionMaking/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DecisionMaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Decison Making/ Conditional Statements
+                1. If-else
+                2. If-else if-else
+                shorthand: ternary/conditional operator
+                3. switch
+            */
+
+            int a = 50 , b = 40;
+            // c = 10;
+
+            //Ternary = 3 Operator
+
+            //Condition ? true : false;
+            String result = (a > b) ? "A is Greater" : "B is Greater";
+
+            Console.WriteLine(result);
+
+            /*
+            if(a > b && a > c)
+            {
+                Console.WriteLine("A is greater");
+            }
+            else if(b > c)
+            {
+                Console.WriteLine("B is greater");
+            }
+            else {
+                Console.WriteLine("C is greater");
+            }*/
+
+            /*
+            if(a > b)
+            {
+                Console.WriteLine("A is greater");
+            }
+            else {
+                Console.WriteLine("B is greater");
+            }*/
+
+        }
+    }
+}
---
+++ SwappingNumbers/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace SwappingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //a = 10, b = 20
+            //a = 20 b = 10
+            /*
+            Swapping has two types:
+            1. Using third variable
+            2. Without using third variable
+            */
+
+            //1. Using third variable
+            /*
+            int a = 5, b = 7;
+
+            Console.WriteLine("{0} and {1}", a, b);
+
+            int temp = a; // 5
+            a = b; //  a = 7
+            b = temp; // b = 5
+
+            Console.WriteLine("{0} and {1}", a, b);*/
+
+            //2. Without using third variable
+
+            int a = 5, b = 10;
+
+            //1st way
+            /*
+            Console.WriteLine("{0} and {1}", a, b);
+
+            a = a + b; // a = 15
+            b = a - b; // 15 - 10 = 5 = b
+            a = a - b; // 15 - 5 = 10 = a
+
+            Console.WriteLine("{0} and {1}", a, b);
+            */
+
+            //2nd Way
+            /*
+            Console.WriteLine("{0} and {1}", a, b);
+
+            a = a * b; // a = 50
+            b = a / b; // 50 / 10 = 5 = b
+            a = a / b; // 50 / 5 = 10 = a
+
+            Console.WriteLine("{0} and {1}", a, b);
+            */
+
+            //3rd Way
+
+            Console.WriteLine("{0} and {1}", a, b);
+
+            // a = 5 =  0101
+            // b = 10 = 1010 ^
+            //            1111 = 15 ^
+            //            0101 = 5 ^
+            //            1010 = 10
+
+            a = a ^ b; // a = 15
+            b = a ^ b; // 15 ^ 10 = 5 = b
+            a = a ^ b; // 15 ^ 5 = 10
+
+            Console.WriteLine("{0} and {1}", a, b);
+
+        }
+    }
+}
