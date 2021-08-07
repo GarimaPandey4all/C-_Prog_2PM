2b7cf20c7efa37269f1b2e2accd4fb991742dd69
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-07 17:51:01 +0530

    Add files via upload

---
+++ TernaryOperator-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TernaryOperator_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 70, b = 50, c = 30;
+
+           String result =  (a > b && a > c) ? "A is Greater" : ((b > c) ? "B is Greater" : "C is Greater");
+
+           Console.WriteLine(result);
+        }
+    }
+}
