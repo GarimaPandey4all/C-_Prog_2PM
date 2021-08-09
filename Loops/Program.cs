7907234875a6a1ceed0650fd438ebe037250acbb
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-09 19:16:24 +0530

    Add files via upload

---
+++ Loops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Loops:
+            1. for loop
+            2. while loop
+            3. do-while loop
+            */
+            //C# loop: foreach loop, only use to read the array
+
+            //loop/repetition/iteration: execute multiple statements
+
+            // int i = 1; Initialization
+            // i <= 10; Condition
+            //Loop body execution
+            // i++ Increment/decrement
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Hello World");
+            }
+        }
+    }
+}
---
+++ Switch/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //switch - menu driven program
+
+            int num1, num2;
+            
+            Console.WriteLine("Press 1. Addition");
+            Console.WriteLine("Press 2. Subtraction");
+            Console.WriteLine("Press 3. Division");
+            Console.WriteLine("Press 4. Multiplication");
+            Console.WriteLine("Enter your choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            
+            switch(choice)
+            {
+                case 1:
+
+                Console.WriteLine("Enter value for number 1");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                    
+                Console.WriteLine("Enter value for number 2");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Addition is: "+(num1 + num2));
+
+                break;
+
+                case 2:
+                Console.WriteLine("Enter value for number 1");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                
+                Console.WriteLine("Enter value for number 2");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Subtraction is: "+(num1 - num2));
+
+                break;
+
+                case 3:
+                Console.WriteLine("Enter value for number 1");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                
+                Console.WriteLine("Enter value for number 2");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Division is: "+(num1 / num2));
+
+                break;
+
+                case 4:
+                Console.WriteLine("Enter value for number 1");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                
+                Console.WriteLine("Enter value for number 2");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Multiplication is: "+(num1 * num2));
+
+                break;
+
+                default:
+                Console.WriteLine("Invalid Choice");
+                break;
+            }
+            Console.WriteLine("Outside the Switch Block");
+        }
+    }
+}
