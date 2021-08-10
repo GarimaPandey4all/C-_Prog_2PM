05e3ed5cb3dc2698c6715e737ee8ea0988b8a0b1
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-10 15:01:42 +0530

    Add files via upload

---
+++ DoWhileLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+
+            Console.WriteLine("Enter any number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            do {
+                Console.WriteLine("{0} * {1} = {2}", n, i, n * i);
+                i++;
+            }while(i <= 10);
+        }
+    }
+}
---
+++ ForLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("Enter any number to be print the table:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} * {1} = {2}", n, i, n * i);
+            }
+        }
+    }
+}
