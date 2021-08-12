44979c01549a47b31c47cedbc7394b2bf468389f
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-12 20:01:50 +0530

    Add files via upload

---
+++ MathFunctions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MathFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any value for a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter any value for b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Math.Max(a, b)); // Maximum Value 
+
+            Console.WriteLine(Math.Min(a, b)); // Minimum Value
+
+            Console.WriteLine(Math.Sqrt(9));
+
+            Console.WriteLine(Math.Abs(-5.7)); // Absolute Positive Value
+
+            Console.WriteLine(Math.Round(45.8));
+
+        }
+    }
+}
---
+++ String/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String: Store Text = Collection of Characters: "Hello"
+
+            string name = "Brain Mentors";
+
+            Console.WriteLine(name);
+            Console.WriteLine(name.Length); // Length = String Length
+            Console.WriteLine(name.ToLower());
+            Console.WriteLine(name.ToUpper());
+
+            string firstName = "Ram";
+            string lastName = "Kumar";
+
+            //string fullName = firstName + lastName; // + : Concatenation / Joining
+
+            //string fullName = string.Concat(firstName, lastName);
+
+            //String Interpolation
+
+            string fullName = $"full name is: {firstName} {lastName}";
+
+            Console.WriteLine(fullName);
+
+            string text = "Hello";
+
+            Console.WriteLine(text[4]); // [] - square bracket/subscript operator // 0 to 4 index numbers
+
+            Console.WriteLine(text.IndexOf("o"));
+
+            string txt = "Ram Kumar";
+
+            int pos = txt.IndexOf("K");
+
+            string subString = txt.Substring(pos);
+
+            Console.WriteLine(subString);
+
+            string a = "10", b = "20";
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
