ad4ef9535cff9a0508f43aaa6cace7192ff1a1d7
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-13 14:59:53 +0530

    Add files via upload

---
+++ Pattern-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pattern_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+
+            for(i = 1; i <= 5; i++) // rows // outer // 5
+            {
+                for(j = 1; j <= 5; j++) // cols // inner // 5 * 5 = 25
+                {
+                    //Console.Write("*");
+                    //Console.Write(i);
+                    Console.Write(j);
+                }
+
+                Console.WriteLine(); // new line, Enter
+            }
+            
+        }
+    }
+}
---
+++ Pattern-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pattern_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char i, j;
+
+            for(i = 'A'; i <= 'E'; i++) 
+            {
+                for(j = 'A'; j <= 'E'; j++) 
+                {
+                    Console.Write(i);
+                    //Console.Write(j);
+                }
+
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
