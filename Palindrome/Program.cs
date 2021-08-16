0a697edeb84473ab71f467bbc9fb4c94e9528277
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-16 21:31:49 +0530

    Add files via upload

---
+++ Palindrome/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            Console.WriteLine("Enter any number that you want to check the given number is palindrome or not:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int temp = n;
+
+            while(n > 0)
+            {
+                // n = 121
+                int remainder = n % 10; // 121 % 10 = 1, 12 % 10 = 2, 1 % 10 = 1
+                sum = sum * 10 + remainder; // 0 * 10 + 1 = 1, 1 * 10 + 2 = 12, 12 * 10 + 1 = 121
+                n = n / 10; // 121 / 10 = 12, 12 / 10 = 1, 1 / 10 = 0
+            }
+
+            n = temp;
+
+            if(sum == n)
+            {
+                Console.WriteLine("Number is Palindrome");
+            }
+            else {
+                Console.WriteLine("Number is not Palindrome");
+            }
+        }
+    }
+}
---
+++ SumofDigits/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SumofDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            Console.WriteLine("Enter any number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int temp = n;
+
+            while(n > 0)
+            {
+                // n = 121 = 4
+                int remainder = n % 10;  // 1
+                sum = sum + remainder;  // 0 + 1 = 1, 1 + 2  = 3, 3 + 1 = 4
+                n = n / 10;  // 12
+            }
+
+            Console.WriteLine("Sum of Digits: "+sum);
+
+        }
+    }
+}
