5723fcb7473d42538951fa9fd4407d7d8411a203
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-24 21:11:40 +0530

    Add files via upload

---
+++ Array-1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Array_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array: It is used to store multiple values in a single variable.
+            // It is collection of similar types of data/values.
+            // It is also called homogeneous mixture.
+
+            // [] - Array's Symbol
+
+            int[] lists = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // array's size = 5, 10
+
+            Console.WriteLine(lists[2]);
+            Console.WriteLine(lists[0]);
+
+            Console.WriteLine(lists.Length); // 10
+            
+            for(int i = 0; i < lists.Length; i++)
+            {
+                Console.Write(lists[i]+" ");
+            }
+        }
+    }
+}
