174b716ec90fe23aa35e81351e643e73b39192fd
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-25 20:06:39 +0530

    Add files via upload

---
+++ Array-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Array_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {20, 10, 30, 40, 50};
+
+            Array.Sort(arr); // Array Sort: ascending order
+
+            // for each loop - array read
+            foreach(int i in arr)
+            {
+                Console.Write(i+" ");
+            }
+
+            Console.WriteLine();
+
+            for(int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i]+" ");
+            }
+
+            Console.WriteLine("Maximum Value is: "+ arr.Max());
+            Console.WriteLine("Minimum Value is: "+ arr.Min());
+            Console.WriteLine("Addition is: "+ arr.Sum());
+        }
+    }
+}
---
+++ Array-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Array_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5]; // array declare
+
+            Console.WriteLine("Enter values in an Array:");
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Values in an Array are:");
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i]+" ");
+            }
+
+        } 
+    }
+}
