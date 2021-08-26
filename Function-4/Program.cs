e93973a917bd7b92b360460c6f4fd940a94ce8d1
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-26 15:03:11 +0530

    Add files via upload

---
+++ Function-4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Function_4
+{
+    class Program
+    {
+        // 4. Function with arguments/parameters and with return type
+
+        static int Sum(int a, int b)
+        {
+            return (a + b);
+        }
+
+        static void Main(string[] args)
+        {
+            int result = Sum(10, 20);
+            Console.WriteLine("Addition is: "+result);
+        }
+    }
+}
