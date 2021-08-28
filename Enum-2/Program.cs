f71e22a4c2ded05e9c4ff887c3520f88b3a55cce
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-28 14:43:33 +0530

    Add files via upload

---
+++ Enum-2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Enum_2
+{
+    class Program
+    { // RGB Format
+        enum colors {
+            red,  // 0
+            green, // 1
+            blue // 2
+        }
+
+        static void Main(string[] args)
+        {
+           // colors myColor = colors.red;
+           colors myColor = colors.blue;
+
+            switch(myColor)
+            {
+                case colors.red:
+                Console.WriteLine("Red Color");
+                break;
+
+                case colors.green:
+                Console.WriteLine("Green Color");
+                break;
+
+                case colors.blue:
+                Console.WriteLine("Blue Color");
+                break;
+
+                default:
+                Console.WriteLine("Invalid Color");
+                break;
+            }
+        }
+    }
+}
---
+++ Enum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        enum week {
+                Monday = 1, // 0
+                Tuesdday, // 1
+                Wednesday, // 2
+                Thursday, // 3
+                Friday, // 4
+                Saturday, // 5
+                Sunday // 6
+        }  
+
+        static void Main(string[] args)
+        {
+            //Enum: Enumeration is a special class in C#, the represents a group of contants.
+            // week today = week.Saturday;
+            // Console.WriteLine(today);
+
+            int today = (int)week.Friday;
+            Console.WriteLine(today);
+        }
+    }
+}
